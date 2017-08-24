6bf0f412799b01182e17977ca9b88f18726cf74d
Josh Beard <josh@jlbeard.com>
2017-08-24 19:30:36 -0400

    Tokenized repo name and added echo for testing.

--- deploy/travis-push.sh
+++ deploy/travis-push.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
 setup_git() {
+  echo "setup_git"
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
 commit_website_files() {
+  echo "commit_website_files"
   git checkout -b pages
   git add . platforms/browser/www
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/jbeard/dqxt-pages.git
+  echo "upload_files"
+  git remote add origin-pages https://${GH_TOKEN}@github.com/${GH_REPO}
   git push --quiet --set-upstream origin-pages pages 
 }
 
