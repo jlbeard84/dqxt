8725012037ba558f360124ad573e0ba8ad81ebe0
Josh Beard <josh@jlbeard.com>
2017-08-24 19:15:11 -0400

    Update travis-push.sh

--- deploy/travis-push.sh
+++ deploy/travis-push.sh
@@ -12,7 +12,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/jbeard/dqxt-pages.git > /dev/null 2>&1
+  git remote add origin-pages https://${GH_TOKEN}@github.com/jbeard/dqxt-pages.git
   git push --quiet --set-upstream origin-pages pages 
 }
 
