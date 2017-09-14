394ee7c6aa941d0cc92c81f2ec7adcf6f456d432
Josh Beard <josh@jlbeard.com>
2017-09-14 11:55:52 -0400

    Added menu.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,41 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { MenuController, Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import * as Pages from '../pages';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = Pages.MenuIndexPage;
+  
+  @ViewChild(Nav) 
+  public nav: Nav;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  public rootPage:any = Pages.MenuIndexPage;
+  public pages: Array<any> = [];
+
+  constructor(
+    menuController: MenuController,
+    platform: Platform, 
+    statusBar: StatusBar, 
+    splashScreen: SplashScreen) {
+
+    this.pages.push(
+      { title: "Menu Translations", component: Pages.MenuIndexPage } 
+    );
+
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
+
+  public openPage(page: any): void {
+    // reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+}
 }
 
